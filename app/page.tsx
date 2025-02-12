30aaeaaa0baf7d23bb2f4ea4dc2548f309d7bb9b
BensonRaro <bensonraro22@gmail.com>
2025-02-13 01:29:25 +0300

    properties fix

--- app/page.tsx
+++ app/page.tsx
@@ -13,16 +13,24 @@ export default async function Home() {
       <div>
         <h1 className="text-xl font-bold text-primary">Featured Properties</h1>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mt-3">
-          {properties.map(async (property) => {
-            const company = await db.query.users.findFirst();
-            return (
-              <PropartyCard
-                property={property}
-                company={company}
-                key={property.id}
-              />
-            );
-          })}
+          {properties.length === 0 ? (
+            <div className="mt-3 text-2xl ml-2 font-bold italic">
+              No properties found
+            </div>
+          ) : (
+            <>
+              {properties.map(async (property) => {
+                const company = await db.query.users.findFirst();
+                return (
+                  <PropartyCard
+                    property={property}
+                    company={company}
+                    key={property.id}
+                  />
+                );
+              })}
+            </>
+          )}
         </div>
       </div>
     </div>
